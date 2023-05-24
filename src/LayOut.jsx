e6c3df996fad7d85868a9cde460696d7ea221f8b
VishnuKV9933 <109938148+VishnuKV9933@users.noreply.github.com>
2023-05-24 13:12:49 +0530

    13th commit

--- src/LayOut.jsx
+++ src/LayOut.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState ,useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Outlet } from "react-router";
 import Navigation from "./Components/Navigation";
 import ProfileCard from "./Components/ProfileCard";
@@ -9,13 +9,15 @@ import { UserContext } from "./Context/UserContext";
 import { ProfileCardUrlContext } from "./Context/ProflePicContext";
 import { baseUrl, defaultProfilePicUrl } from "./Utility/utility";
 function LayOut() {
-const {profileCardName,setProfileCardName}=useContext(UserContext)
-const {profilCardUrl,setProfileCardUrl} = useContext(ProfileCardUrlContext)
+  const { profileCardName, setProfileCardName } = useContext(UserContext);
+  const { profilCardUrl, setProfileCardUrl } = useContext(
+    ProfileCardUrlContext
+  );
   const userId = JSON.parse(localStorage.getItem("userId"));
   const [suggestion, setSuggestion] = useState([]);
   const [user, setUser] = useState(null);
-  const [suggestionDisplayController, setSuggestionDisplayController] =useState(true);
-  
+  const [suggestionDisplayController, setSuggestionDisplayController] =
+    useState(true);
 
   useEffect(() => {
     const getUser = async () => {
@@ -24,13 +26,16 @@ const {profilCardUrl,setProfileCardUrl} = useContext(ProfileCardUrlContext)
       });
 
       setUser(user.data);
-      setProfileCardName(user.data.username)
-       setProfileCardUrl(user?.data?.profilePicture?user?.data?.profilePicture:defaultProfilePicUrl)
+      setProfileCardName(user.data.username);
+      setProfileCardUrl(
+        user?.data?.profilePicture
+          ? user?.data?.profilePicture
+          : defaultProfilePicUrl
+      );
     };
 
     getUser();
   }, [profileCardName]);
-
 
   useEffect(() => {
     axios
@@ -58,97 +63,89 @@ const {profilCardUrl,setProfileCardUrl} = useContext(ProfileCardUrlContext)
     setSuggestionDisplayController(searchData);
   };
 
-
   return (
-<div className="flex  w-full ">
-{/* ---------------------fixed------------------------ */}
+    <div className="flex  w-full ">
+      {/* ---------------------fixed------------------------ */}
 
-<div className="fixed w-full flex">
-  <div
-    className="bg-white   overflow-hidden
+      <div className="fixed w-full flex">
+        <div
+          className="bg-white   overflow-hidden
 top-0 left-0 right-0
 xl:w-w23 h-screen xl:pr-9 xl:pl-2 pt-1
 lg:w-w30 lg:pl-4 lg:pr-8
 md:w-w25 md:px-2 md:w-w30 md:block hidden "
-  >
-    <div>
-      {" "}
-      <ProfileCard name={profileCardName} url={profilCardUrl}/>{" "}
-    </div>
-    <div className="mt-4">
-      <Navigation />
-    </div>
-  </div>
-  {/* -----------------------------layout-------------------- */}
+        >
+          <div>
+            {" "}
+            <ProfileCard name={profileCardName} url={profilCardUrl} />{" "}
+          </div>
+          <div className="mt-4">
+            <Navigation />
+          </div>
+        </div>
+        {/* -----------------------------layout-------------------- */}
 
-  <div className="md:w-w50 sm:w-full"></div>
+        <div className="md:w-w50 sm:w-full"></div>
 
-  {/* -----------------------------right-------------------- */}
+        {/* -----------------------------right-------------------- */}
 
-  <div className=" w-w30 hidden md:block lg:w-w27">
-    <div className="w-full  h-full ">
-      <div className="">
+        <div className=" w-w30 hidden md:block lg:w-w27">
+          <div className="w-full  h-full ">
+            <div className="">
+              <Search searchPasser={searchPasser} />
+            </div>
 
-      <Search  searchPasser={searchPasser}/>
-      </div>
-
-      <div className="w-full h-5/6  flex flex-col z-80 px-4 ">
-        <div className=" mb-12">
-          {!suggestionDisplayController && (
-            <div>
-              <div className="ml-4 font-semibold text-lg	">Suggestion</div>
-              <div className="h-fit " >
-
-              <div>
-                {suggestion?.map((elem) => {
-                  return (
-                    <Suggestion
-                      key={elem._id}
-                      suggestion={elem}
-                      suggestionFunction={suggestionFunction}
-                      user={user}
-                    />
-                  );
-                })}
-              </div>
-
+            <div className="w-full h-5/6  flex flex-col z-80 px-4 ">
+              <div className=" mb-12">
+                {!suggestionDisplayController && (
+                  <div>
+                    <div className="ml-4 font-semibold text-lg	">Suggestion</div>
+                    <div className="h-fit ">
+                      <div>
+                        {suggestion?.map((elem) => {
+                          return (
+                            <Suggestion
+                              key={elem._id}
+                              suggestion={elem}
+                              suggestionFunction={suggestionFunction}
+                              user={user}
+                            />
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
-          )}
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
 
-{/* ---------------------fixed------------------------ */}
+      {/* ---------------------fixed------------------------ */}
 
-{/* --------------------------------------------- */}
-<div
-  className="bg-red-600   overflow-hidden
+      {/* --------------------------------------------- */}
+      <div
+        className="bg-red-600   overflow-hidden
 top-0 left-0 right-0
 xl:w-w23 h-screen xl:pr-9 xl:pl-2 pt-1
 lg:w-w30 lg:pl-4 lg:pr-8
 md:w-w25 md:px-2 md:w-w30 md:block hidden "
-></div>
+      ></div>
 
-<div className=" z-10 md:w-w50 sm:w-full ">
-  <div className=" px-6">
-    {/* -----------------------------layout-------------------- */}
+      <div className=" z-10 md:w-w50 sm:w-full ">
+        <div className=" px-6">
+          {/* -----------------------------layout-------------------- */}
 
-    <Outlet />
+          <Outlet />
 
-    {/* -----------------------------right-------------------- */}
-  </div>
-</div>
+          {/* -----------------------------right-------------------- */}
+        </div>
+      </div>
 
-<div className="w-w30 hidden md:block lg:w-w27 mt-16"></div>
-</div>
-
+      <div className="w-w30 hidden md:block lg:w-w27 mt-16"></div>
+    </div>
   );
 }
 
 export default LayOut;
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import UserContext from './Context/UserContext';
-import ProfileContext from './Context/ProfileContext';
-import CommentContext from './Context/CommetContext'
-import authReducer from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import UserContext from "./Context/UserContext";
+import ProfileContext from "./Context/ProfileContext";
+import CommentContext from "./Context/CommetContext";
+import authReducer from "./redux/store";
 
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import {
   persistStore,
   persistReducer,
@@ -19,60 +19,51 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
-} from "redux-persist"
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
-import User from './Context/UserContext';
-import ProflePicContext from './Context/ProflePicContext';
-import CurrentChatContext from './Context/ChatContext';
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import User from "./Context/UserContext";
+import ProflePicContext from "./Context/ProflePicContext";
+import CurrentChatContext from "./Context/ChatContext";
 
-const persistConfig = { key: "root", storage, version: 1, blacklist: ['chats', 'currentChat', 'messages', 'loading']};
-const persistedReducer = persistReducer(persistConfig, authReducer)
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+  blacklist: ["chats", "currentChat", "messages", "loading"],
+};
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [  FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-      }
-    })
-  
-})
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root')); 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <ProfileContext>
-<Provider store={store}>
-<PersistGate loading={null} persistor={persistStore(store)}>
-
-         <UserContext>
-          <CommentContext>
-            
-          <User>
-            <ProflePicContext>
-           <CurrentChatContext>
-
-         <App/>
-           </CurrentChatContext>
-
-           
-
-            </ProflePicContext>
-          </User>
-          </CommentContext>
-         </UserContext>
-
-
-</PersistGate>
-</Provider>
-
-        </ProfileContext>
-  
-   
-    </React.StrictMode> 
+  <React.StrictMode>
+    <ProfileContext>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistStore(store)}>
+          <UserContext>
+            <CommentContext>
+              <User>
+                <ProflePicContext>
+                  <CurrentChatContext>
+                    <App />
+                  </CurrentChatContext>
+                </ProflePicContext>
+              </User>
+            </CommentContext>
+          </UserContext>
+        </PersistGate>
+      </Provider>
+    </ProfileContext>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
